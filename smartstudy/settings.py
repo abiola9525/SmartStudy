66d0f5b226c0d29cedddc4500c3a2215bff97d87
abiola9525 <omotosoabiola9525@gmail.com>
2025-01-11 15:14:59 +0100

    initial

--- smartstudy/settings.py
+++ smartstudy/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'smart-study.up.railway.app']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'smartstudy.up.railway.app']
 
 
 # Application definition
